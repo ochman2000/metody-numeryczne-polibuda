905672d9dc0f91a6ab07ed1812ce069351091ea6
ochman2000@gmail.com <ochman2000@gmail.com@6b43cadb-222c-6630-e340-f19cb0564815>
2013-10-12 13:22:15 +0000

    Wersja 1.0.2

--- Lab01/src/lab01/Funkcja.java
+++ Lab01/src/lab01/Funkcja.java
@@ -1,75 +1,82 @@
 package lab01;
 
 public class Funkcja {
-	
-	private static double e = Math.E;
-	private int index;
-	
-	public Funkcja(int numerWybranejFunkcji) {
-		index=numerWybranejFunkcji;
-	}
-	
-	public double getFunkcja(double x){
-		switch (index) {
-		case 1:
-			return 3*x + Math.sin(x) - Math.exp(x);
-		case 2:
-			return 2*Math.exp(-x) - Math.sin(x);
-		case 3:
-			return Math.pow(2, x) - 3*x;
-		default:
-			return Double.NaN;
-		}
-	}
-	public double getPochodna(double x){
-		switch (index) {
-		case 1:
-			return 3-Math.exp(x)+Math.cos(x);
-		case 2:
-			return (-2)*Math.exp(-x)-Math.cos(x);
-		case 3:
-			return Math.pow(2, x)*Math.log(2)-3;
-		default:
-			return Double.NaN;
-		}
-	}
-	/**
-	 * <b>Równanie: </b>3x + sin(x) - e^x = 0<br/>
-	 * <b>Pierwiastki: </b>	x = 0.3604<br/><br/>
-	 */
-	public static double funkcjaNumer1(double x) {
-		return 3*x + Math.sin(x) - Math.exp(x);
-	}
-	/**
-	 * <b>Pochodna: </b> -e^x + cos(x) + 3
-	 */
-	public static double pochodnaNumer1(double x) {
-		return 3-Math.exp(x)+Math.cos(x);
-	}
-	/**
-	 * <b>Równanie: </b>2e^(-x) - sin(x) = 0<br/>
-	 * <b>Pierwiastki: </b> x = 0.921028<br/><br/>
-	 */
-	public static double funkcjaNumer2(double x) {
-		return 2*Math.exp(-x) - Math.sin(x);
-	}
-	/**
-	 * <b>Pochodna: </b> -2 * e^(-x)- cos(x)
-	 */
-	public static double pochodnaNumer2(double x) {
-		return (-2)*Math.exp(-x)-Math.cos(x);
-	}
-	/**
-	 * <b>Równanie: </b> 2^x - 3x = 0 <br/>
-	 * <b>Pierwiastki: </b> x = 0.435<br/>x = 0.332<br/><br/>
-	 */
-	public static double funkcjaNumer3(double x) {
-		return Math.pow(2, x) - 3*x;
-	}
-	/**
-	 * <b>Pochodna: </b> 2^x * log(2) - 3
-	 */
-	public static double pochodnaNumer3(double x) {
-		return Math.pow(2, x)*Math.log(2)-3;
-	}
+
+    private static double e = Math.E;
+    private int index;
+
+    public Funkcja(int numerWybranejFunkcji) {
+        index = numerWybranejFunkcji;
+    }
+
+    public double getFunkcja(double x) {
+        switch (index) {
+            case 1:
+                return 3 * x + Math.sin(x) - Math.exp(x);
+            case 2:
+                return 2 * Math.exp(-x) - Math.sin(x);
+            case 3:
+                return Math.pow(2, x) - 3 * x;
+            default:
+                return Double.NaN;
+        }
+    }
+
+    public double getPochodna(double x) {
+        switch (index) {
+            case 1:
+                return 3 - Math.exp(x) + Math.cos(x);
+            case 2:
+                return (-2) * Math.exp(-x) - Math.cos(x);
+            case 3:
+                return Math.pow(2, x) * Math.log(2) - 3;
+            default:
+                return Double.NaN;
+        }
+    }
+
+    /**
+     * <b>Równanie: </b>3x + sin(x) - e^x = 0<br/>
+     * <b>Pierwiastki: </b>	x = 0.3604<br/><br/>
+     */
+    public static double funkcjaNumer1(double x) {
+        return 3 * x + Math.sin(x) - Math.exp(x);
+    }
+
+    /**
+     * <b>Pochodna: </b> -e^x + cos(x) + 3
+     */
+    public static double pochodnaNumer1(double x) {
+        return 3 - Math.exp(x) + Math.cos(x);
+    }
+
+    /**
+     * <b>Równanie: </b>2e^(-x) - sin(x) = 0<br/>
+     * <b>Pierwiastki: </b> x = 0.921028<br/><br/>
+     */
+    public static double funkcjaNumer2(double x) {
+        return 2 * Math.exp(-x) - Math.sin(x);
+    }
+
+    /**
+     * <b>Pochodna: </b> -2 * e^(-x)- cos(x)
+     */
+    public static double pochodnaNumer2(double x) {
+        return (-2) * Math.exp(-x) - Math.cos(x);
+    }
+
+    /**
+     * <b>Równanie: </b> 2^x - 3x = 0 <br/>
+     * <b>Pierwiastki: </b> x = 0.435<br/>x = 0.332<br/><br/>
+     */
+    public static double funkcjaNumer3(double x) {
+        return Math.pow(2, x) - 3 * x;
+    }
+
+    /**
+     * <b>Pochodna: </b> 2^x * log(2) - 3
+     */
+    public static double pochodnaNumer3(double x) {
+        return Math.pow(2, x) * Math.log(2) - 3;
+    }
 }
