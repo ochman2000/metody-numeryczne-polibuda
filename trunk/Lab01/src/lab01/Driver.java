ccc8b6cbe81253d3b4f02cd5c17ccc611acb56f1
ochman2000@gmail.com <ochman2000@gmail.com@6b43cadb-222c-6630-e340-f19cb0564815>
2013-10-16 23:35:33 +0000

    Wersja 1.0.3 z zakomentowaną animacją Timeline

---
+++ trunk/Lab01/src/lab01/Driver.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab01;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lukasz Ochmanski
+ */
+public class Driver extends Application {
+    GridPane grid;
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome");
+        initLayout();
+        Scene scene = new Lab01(grid);
+        scene.getStylesheets().add("linechartsample/Chart.css"); 
+        primaryStage.setScene(scene);
+        primaryStage.show();
+//        final Timeline timeline = new Timeline();
+//        timeline.setCycleCount(Timeline.INDEFINITE);
+//        timeline.setAutoReverse(true);
+//        final KeyValue kv = new KeyValue(grid.translateYProperty(), 20);
+//        final KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
+//        timeline.getKeyFrames().add(kf);
+//        timeline.play();
+
+    }
+    
+    private void initLayout() {
+        grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+    }
+    
+    
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+}
